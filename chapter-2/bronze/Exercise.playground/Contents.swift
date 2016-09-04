ad352b9206f678b2143d5dc270a00a84de6e3f6e
Chris Miller <cmiller11101@gmail.com>
2016-09-04 17:09:59 +0900

    Chapter 2 woot

---
+++ chapter-2/bronze/Exercise.playground/Contents.swift
@@ -0,0 +1,17 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+let townName: String = "Townvilleshipboro"
+let townNumberOfStoplights: Int = 4
+
+var townPopulation: Int
+var townUnemployment: Double = 123.45
+
+townPopulation = 1234
+townUnemployment = 33.4
+
+let townDescription =
+"\(townName) has a population of \(townPopulation), \(townNumberOfStoplights) stoplights, and the unemployment rate is \(townUnemployment)%."
+
+print(townDescription)
