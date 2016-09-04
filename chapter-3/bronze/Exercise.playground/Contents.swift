e712b4b431f6f02630f3ecc05513ea0fc820af26
Chris Miller <cmiller11101@gmail.com>
2016-09-04 17:18:37 +0900

    Chapter 3 - code from chapter

---
+++ chapter-3/bronze/Exercise.playground/Contents.swift
@@ -0,0 +1,17 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+var population: Int = 5442
+var message: String
+var hasPostOffice: Bool = true
+
+if population < 10000 {
+    message = "\(population) is a small town!"
+} else if population >= 10000 && population < 50000 {
+    message = "\(population) is a medium town!"
+} else {
+    message = "\(population) is pretty big!"
+}
+
+print(message)
