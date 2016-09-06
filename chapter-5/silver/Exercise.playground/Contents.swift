a32a45b3139e85d3ee08542afcf4b1c5cc96b310
Chris Miller <cmiller11101@gmail.com>
2016-09-06 08:46:50 +0200

    Oops, I skipped to chapter 9 and thought it was 5...

--- chapter-5/silver/Exercise.playground/Contents.swift
+++
@@ -1,5 +0,0 @@
-//: Playground - noun: a place where people can play
-
-import Cocoa
-
-var toDoList = [" Take out garbage", "Pay bills", "Cross off finished items"]
--- chapter-9/silver/Exercise.playground/Contents.swift
+++ chapter-9/silver/Exercise.playground/Contents.swift
@@ -4,4 +4,4 @@ import Cocoa
 
 var toDoList = [" Take out garbage", "Pay bills", "Cross off finished items"]
 
-print(toDoList.count)
+for item in 
